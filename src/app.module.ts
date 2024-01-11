52e3c5b6a4b9629d07a0c4559bde7780624b9862
Gaspar Sio <sio.gaspar@gmail.com>
2024-01-11 12:51:04 +0100

    create a module to encapsulate service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffessController } from './coffess/coffess.controller';
-import { CoffessService } from './coffess/coffess.service';
+import { CoffessModule } from './coffess/coffess.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CoffessController],
-  providers: [AppService, CoffessService],
+  imports: [CoffessModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
