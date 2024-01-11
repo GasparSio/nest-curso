83d86033b9212fea6acdf63d30dd977d2bc5a61a
Gaspar Sio <sio.gaspar@gmail.com>
2024-01-11 10:29:05 +0100

    created the patch and delete controller

--- src/coffess/coffess.controller.ts
+++ src/coffess/coffess.controller.ts
@@ -1,18 +1,25 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
 
 @Controller('coffess')
 export class CoffessController {
     @Get()
-    findAll(@Res() response){
-        response.status(200).send('This action returns all coffess');
+    findAll(){
+        return 'This action returns all coffess';
     }
     @Get(':id')
     findOne(@Param('id') id: string){
         return `This action returns the ${id}`
     }
     @Post()
-    @HttpCode(HttpStatus.GONE)
     create(@Body() body){
         return body;
+    }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body){
+        return `This action update the item with the ID: ${id}`
+    }
+    @Delete(':id')
+    delete(@Param('id') id: string){
+        return `This action delete the item with the ID: ${id}`
     }
 }
