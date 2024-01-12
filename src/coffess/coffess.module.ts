2e08d5d8ab21f995c6bd1c6db7bd7182a95e2196
Gaspar Sio <sio.gaspar@gmail.com>
2024-01-12 12:39:18 +0100

    created a many to many relation betw. two entities

--- src/coffess/coffess.module.ts
+++ src/coffess/coffess.module.ts
@@ -3,9 +3,10 @@ import { CoffessController } from './coffess.controller';
 import { CoffessService } from './coffess.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity.ts';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Coffee])],
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
     controllers: [CoffessController],
     providers: [CoffessService]
 })
