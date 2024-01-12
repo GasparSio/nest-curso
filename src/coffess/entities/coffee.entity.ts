dd104705852d2322454af34f64bcd5317d912224
Gaspar Sio <sio.gaspar@gmail.com>
2024-01-12 14:19:20 +0100

    adding indexes to entities

--- src/coffess/entities/coffee.entity.ts
+++ src/coffess/entities/coffee.entity.ts
@@ -1,11 +1,13 @@
-import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, Index, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Flavor } from "./flavor.entity.ts";
 
+@Index(['name', 'brand'])
 @Entity()
 export class Coffee {
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Index()
     @Column()
     name: string;
 
