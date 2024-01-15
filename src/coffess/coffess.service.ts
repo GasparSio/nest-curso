a4b80360bd0244cce6c8fdfe93aa876fb053e21c
Gaspar Sio <sio.gaspar@gmail.com>
2024-01-15 16:12:32 +0100

    first 2e2 test

--- src/coffess/coffess.service.ts
+++ src/coffess/coffess.service.ts
@@ -4,7 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { Flavor } from './entities/flavor.entity.ts';
-import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query/pagination-query.dto';
 import { of } from 'rxjs';
 
 @Injectable()
